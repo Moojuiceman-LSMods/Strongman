5e6b5cc247f23a8a5b865a60e56261d232cb690e
Alex Barrett <moojuiceman@hotmail.co.uk>
2022-07-01 22:08:08 +0100

    Handle 0.08.02 plasterboard changes

--- Plugin.cs
+++ Plugin.cs
@@ -17,12 +17,14 @@ namespace Strongman
         static ManualLogSource logger;
         static ConfigEntry<int> maxShingles;
         static ConfigEntry<int> maxBricks;
+        static ConfigEntry<int> maxPlasterboard;
         static ConfigEntry<float> weightLimit;
 
         private void Awake()
         {
             maxShingles = Config.Bind("General", "Max Shingles", 20, "Max shingles that can be carried (Vanilla:12)");
             maxBricks = Config.Bind("General", "Max Bricks", 20, "Max bricks that can be carried (Vanilla:11)");
+            maxPlasterboard = Config.Bind("General", "Max Plasterboards", 20, "Max plasterboards that can be carried (Vanilla:8 - only in game version 0.08.02 and up)");
             weightLimit = Config.Bind("General", "Weight Limit", 100000f, "Weight that can be lifted and still sprint/jump (Vanilla:10)");
 
             // Plugin startup logic
@@ -35,12 +37,22 @@ namespace Strongman
 
         [HarmonyPatch(typeof(HandToFront), "Awake")]
         [HarmonyPostfix]
-        static void Awake_Postfix(ref int ___maxShingleAmmo, ref int ___maxBrickAmmo, ref HandToFront.GenericStuffs ____refLibrary)
+        static void Awake_Postfix(HandToFront __instance, ref HandToFront.GenericStuffs ____refLibrary)
         {
-            ___maxShingleAmmo = maxShingles.Value;
-            ___maxBrickAmmo = maxBricks.Value;
-            Array.Resize<Transform>(ref ____refLibrary.shingleHolder.GetComponent<BrickCarrierLogic>().brickLocs, ___maxShingleAmmo);
-            Array.Resize<Transform>(ref ____refLibrary.brickHolder.GetComponent<BrickCarrierLogic>().brickLocs, ___maxBrickAmmo);
+            FieldInfo maxPlasterBoardAmmo = AccessTools.Field(typeof(HandToFront), "maxPlasterBoardAmmo");
+
+            if (maxPlasterBoardAmmo != null)
+            {
+                maxPlasterBoardAmmo.SetValue(__instance, maxPlasterboard.Value);
+            }
+
+            __instance.maxShingleAmmo = maxShingles.Value;
+            __instance.maxBrickAmmo = maxBricks.Value;
+
+            int brickSlotsNeeded = Math.Max(maxBricks.Value, maxPlasterboard.Value);
+
+            Array.Resize<Transform>(ref ____refLibrary.shingleHolder.GetComponent<BrickCarrierLogic>().brickLocs, __instance.maxShingleAmmo);
+            Array.Resize<Transform>(ref ____refLibrary.brickHolder.GetComponent<BrickCarrierLogic>().brickLocs, brickSlotsNeeded);
             for (int i = 0; i < ____refLibrary.shingleHolder.GetComponent<BrickCarrierLogic>().brickLocs.Length; i++)
             {
                 if (____refLibrary.shingleHolder.GetComponent<BrickCarrierLogic>().brickLocs[i] == null)
